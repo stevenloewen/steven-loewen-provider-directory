97d8dc492b9dc136f7dd6590f37bf65ef46f2199
Steven Loewen <steven.c.loewen@gmail.com>
2022-01-13 00:14:31 -0500

    provider profile page built, just need to add read more button functionality

---
+++ src/components/ProviderProfileDetailCard/ProviderProfileDetailCard.js
@@ -0,0 +1,17 @@
+import "./ProviderProfileDetailCard.scss";
+
+import React from 'react';
+
+const ProviderProfileDetailCard = ({image, title, subtitle}) => {
+    return (
+        <div className="provider-profile-detail-card">
+            <img src={image} alt={title} className="provider-profile-detail-card__image" />
+            <div className="provider-profile-detail-card-text">
+                <p className="provider-profile-detail-card-text__title">{title}</p>
+                <p className="provider-profile-detail-card-text__subtitle">{subtitle}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ProviderProfileDetailCard;
--- src/pages/ProviderProfile/ProviderProfile.js
+++ src/pages/ProviderProfile/ProviderProfile.js
@@ -6,19 +6,27 @@ import { useParams } from "react-router-dom";
 
 import { fetchProvider } from "../../api";
 
+import ProviderProfileDetailCard from "../../components/ProviderProfileDetailCard/ProviderProfileDetailCard";
+
 import Chevron from "../../assets/icons/chevron.svg";
+import LocationMap from "../../assets/icons/location-map.svg";
+import Education from "../../assets/icons/education.svg";
+import Language from "../../assets/icons/language.svg";
 
 const ProviderProfile = () => {
   const { id } = useParams();
 
   const [providerProfileData, setProviderProfileData] = useState({});
+  const [languages, setLanguages] = useState([]);
 
   useEffect(() => {
     fetchProvider(id).then((response) => {
       setProviderProfileData(response);
+      setLanguages(response.languages.join(', '));
     });
-  }, []);
+  }, [id]);
   console.log("profile data", providerProfileData);
+//   console.log("languages", providerProfileData.languages[0]);
 
   return (
     <div className="provider-profile">
@@ -40,11 +48,28 @@ const ProviderProfile = () => {
           </p>
         </div>
         <div className="read-more">
-            <p className="read-more__text">Read more</p>
-            <img src={Chevron} alt="chevron" className="read-more__chevron" />
+          <p className="read-more__text">Read more</p>
+          <img src={Chevron} alt="chevron" className="read-more__chevron" />
         </div>
       </div>
-      
+      <div className="provider-profile-details-container">
+        <ProviderProfileDetailCard
+          image={LocationMap}
+          title="Location"
+          subtitle={providerProfileData.location}
+        />
+        <ProviderProfileDetailCard
+          image={Education}
+          title="Education"
+          subtitle={providerProfileData.education}
+        />
+        <ProviderProfileDetailCard
+          image={Language}
+          title="Language"
+          subtitle={languages}
+        />
+      </div>
+      <button className="book">Book with us</button>
     </div>
   );
 };
